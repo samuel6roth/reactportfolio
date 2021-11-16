2460f8454a0bc33e2c570cd1c07a085907157ac9
Samuel Roth <samuel6roth@gmail.com>
2021-11-15 21:00:57 -0600

    Updating resume

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,8 +4,8 @@ import Row from "../components/Row";
 import Col from "../components/Col";
 import bill from "../assets/bill-reminder.png"
 import planner from "../assets/dailyplanner.jpg"
-import blog from "../assets/tech-blog.jpeg"
 import Project from "../components/Project";
+import employeedir from "../assets/employeedir.png"
 
 function Projects() {
   return (
@@ -36,9 +36,9 @@ function Projects() {
         </Row>
         <Row>
           <Col size="md-12">
-          <a href="https://salty-headland-05928.herokuapp.com/" target="_blank" rel="noreferrer">
-            <Project backgroundImage={blog}>
-              <h1>Tech Blog</h1>
+          <a href="https://samuel6roth.github.io/employee-directory/" target="_blank" rel="noreferrer">
+            <Project backgroundImage={employeedir}>
+              <h1>Employee Directory</h1>
             </Project>
           </a>
           </Col>
